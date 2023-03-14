3d24f87676e88b6f736890f56e6d103258fa9446
Heng Ma <maheng1991@hotmail.com>
2023-03-14 11:22:16 -0700

    add framework for md simulation setup and run

---
+++ multirl/sim/amber.py
@@ -0,0 +1,206 @@
+import os
+import subprocess
+# import tempfile
+import MDAnalysis as mda
+
+from .utils import add_hydrogen, remove_hydrogen, missing_hydrogen
+from .utils import build_logger, get_lig_charge, get_lig_name
+
+logger = build_logger()
+
+class AMBER_param(object):
+    """
+    A master function to parameterize complex pdb file with
+    multiple protein chains and possibly ligands, assuming
+    solvent molecules are removed from the system
+    """
+
+    def __init__(
+            self, pdb,
+            add_sol=True,
+            lig_charges={},
+            lig_param_path='',
+            forcefield='ff19SB', 
+            forcefield_rna='OL3',
+            forcefield_dna='OL15',
+            watermodel='opc',
+            padding=20, 
+            cubic=True, 
+            cation='Na+', 
+            n_cations=0, 
+            anion='Cl-', 
+            n_anions='0'):
+
+        self.pdb = pdb
+        self.label = os.path.basename(pdb)[:-4]
+        self.add_sol = add_sol
+        self.lig_charges = lig_charges
+        self.lig_param_path = lig_param_path
+        self.forcefield = forcefield
+        self.forcefield_rna = forcefield_rna
+        self.forcefield_dna = forcefield_dna
+        self.watermodel = watermodel
+        self.padding = padding
+        self.cubic = cubic
+        self.cation = cation
+        self.n_cations = n_cations
+        self.anion = anion 
+        self.n_anions = n_anions
+        self.pdb_dissect()
+
+    def pdb_dissect(self):
+        """
+        function to separate pdb file into each component of
+        the complex
+
+        NOTE: it requires proper chain IDs for each segment
+        """
+        mda_traj = mda.Universe(self.pdb)
+        self.prot_files = []
+        self.lig_files = []
+        # not_na_not_protein = mda_traj.select_atoms('not protein and not nucleic')
+        # if not_na_not_protein.n_atoms == 0: 
+        #     self.prot_files.append(self.pdb)
+        #     return
+
+        # loop over segments in the complex
+        for seg in mda_traj.atoms.segments:
+            protein = seg.atoms.select_atoms('protein')
+            not_protein = seg.atoms.select_atoms('not protein')
+            na_not_protein = not_protein.atoms.select_atoms('nucleic')
+            not_na_not_protein = not_protein.atoms.select_atoms('not nucleic')
+            # print(protein.n_atoms, not_protein.n_atoms)
+            # processing protein
+            if protein.n_atoms != 0:
+                protein_save = f'prot_seg{seg.segid}_{seg.segindex}.pdb'
+                protein.write(protein_save)
+                if not missing_hydrogen(protein_save):
+                    remove_hydrogen(protein_save, protein_save)
+                self.prot_files.append(protein_save)
+
+            # processing na
+            if na_not_protein.n_atoms != 0: 
+                na_not_protein_save = f'na_seg{seg.segid}.pdb'
+                na_not_protein.wrote(na_not_protein_save) 
+                self.prot_files.append(na_not_protein_save)
+            
+            # processing ligands
+            if not_protein.n_atoms != 0:
+                for i, res in enumerate(not_protein.residues):
+                    lig_save = f"lig_seg{seg.segid}_{i}.pdb"
+                    res.atoms.write(lig_save)
+                    self.lig_files.append(lig_save)
+
+    def param_ligs(self):
+        """
+        parameterize the ligand pdb with amber antechamber
+        tools
+
+        possible improvement:
+        """
+        # SCF modifications
+        # antechamber_command = f"""
+        #     antechamber -i {pdb_lig} -fi pdb
+        #     -o lig.mol2 -fo mol2 -c bcc -pf y
+        #     -an y -nc {lig_charge}
+        #     -ek "qm_theory='AM1', grms_tol=0.0005,
+        #     scfconv=1.d-9, ndiis_attempts=1000" """
+        for lig in self.lig_files:
+            lig_tag = os.path.basename(lig)[:-4]
+            lig_name = get_lig_name(lig)
+            if missing_hydrogen(lig):
+                lig = add_hydrogen(lig)
+            if lig_name in self.lig_charges:
+                lig_charge = self.lig_charges[lig_name]
+            else:
+                lig_charge = get_lig_charge(lig)
+            
+            antechamber_command = \
+                f'antechamber -i {lig} -fi pdb -o {lig_tag}.mol2 '\
+                f'-fo mol2 -c bcc -pf y -an y -nc {lig_charge}'
+            subprocess.check_output(antechamber_command, shell=True)
+            param_command = \
+                f'parmchk2 -i {lig_tag}.mol2 -f mol2 -o {lig_tag}.frcmod'
+            subprocess.check_output(param_command, shell=True)
+
+    def param_comp(self):
+        """
+        parameterize the complex
+        """
+        self.get_outputs()
+        # skip if already run
+        if (os.path.exists(self.output_top) and
+            os.path.exists(self.output_inpcrd)):
+            logger.info(f"Topology found, skipping building {os.path.basename(self.output_pdb)}...")
+            return self.output_inpcrd, self.output_top
+        
+        if not self.lig_param_path:
+            self.param_ligs()
+        self.write_tleapIN()
+        subprocess.check_output(f'tleap -f leap.in', shell=True)
+        # checking whether tleap is done
+        if (os.path.exists(self.output_top) and
+            os.path.exists(self.output_inpcrd)):
+            logger.info(f"Successfully built {os.path.basename(self.output_pdb)}...")
+            return self.output_inpcrd, self.output_top
+        else:
+            raise Exception("Leap failed to build topology, check errors...")
+    
+    def get_outputs(self): 
+        self.output_path = os.path.abspath(os.path.dirname(self.pdb))
+        self.output_top = os.path.join(self.output_path, f'{self.label}.prmtop')
+        self.output_inpcrd = os.path.join(self.output_path, f'{self.label}.inpcrd')
+        self.output_pdb = os.path.join(self.output_path, f'{self.label}.pdb')
+
+    def write_tleapIN(self):
+        """
+        produce tleap input file
+        """
+        with open(f'leap.in', 'w+') as leap:
+            # default protein ff
+            leap.write(f"source leaprc.protein.{self.forcefield}\n")
+            leap.write(f"source leaprc.RNA.{self.forcefield_rna}\n")
+            leap.write(f"source leaprc.DNA.{self.forcefield_dna}\n")
+            leap.write("source leaprc.gaff\n")
+            leap.write(f"source leaprc.water.{self.watermodel}\n")
+            # leap.write(f"source leaprc.")
+            leap.write("set default PBRadii mbondi3\n")
+
+            # load proteins
+            prot_insts = []
+            for i, prot in enumerate(self.prot_files):
+                leap_name = f'biom{i}'
+                leap.write(f"{leap_name} = loadPDB {prot}\n")
+                prot_insts.append(leap_name)
+            prot_insts = ' '.join(prot_insts)
+            # leap.write("saveAmberParm rec apo.prmtop apo.inpcrd\n")
+
+            # load ligands
+            lig_insts = []
+            for lig in self.lig_files:
+                lig_tag = os.path.basename(lig)[:-4]
+                if self.lig_param_path:
+                    lig_path_tag = f'{self.lig_param_path}/{lig_tag}'
+                else:
+                    lig_path_tag = lig_tag
+                leap.write(f"{lig_tag} = loadmol2 {lig_path_tag}.mol2\n")
+                leap.write(f"loadAmberParams {lig_path_tag}.frcmod\n")
+                lig_insts.append(lig_tag)
+            lig_insts = ' '.join(lig_insts)
+
+            # leap.write("saveAmberParm lig lig.prmtop lig.inpcrd\n")
+            combine_insts = prot_insts + ' ' + lig_insts
+            leap.write(f"comp = combine {{ {combine_insts} }}\n")
+            if self.add_sol:
+                leap.write(f"solvatebox comp {self.watermodel.upper()}BOX {self.padding}")
+                if self.cubic: 
+                    leap.write(f" iso\n")
+                else: 
+                    leap.write('/n')
+                leap.write(f"addions comp {self.cation} {self.n_cations}\n")
+                leap.write(f"addions comp {self.anion} {self.n_anions}\n")
+            leap.write(f"saveAmberParm comp {self.output_top} {self.output_inpcrd}\n")
+            leap.write(f"savepdb comp {self.output_pdb}\n")
+            leap.write("quit\n")
+        return 1
+
---
+++ multirl/sim/utils.py
@@ -0,0 +1,277 @@
+import os 
+import time
+import yaml
+import logging
+import argparse
+import tempfile
+import subprocess
+import warnings
+
+from typing import Union
+from pathlib import Path
+from typing import Type, TypeVar
+# from pydantic import BaseSettings as _BaseSettings
+
+import parmed as pmd
+import MDAnalysis as mda
+
+from rdkit import Chem
+from mendeleev import element
+from MDAnalysis.analysis import align
+
+
+PathLike = Union[str, Path]
+_T = TypeVar("_T")
+
+
+def build_logger(debug=0):
+    logger_level = logging.DEBUG if debug else logging.INFO
+    logging.basicConfig(level=logger_level, format='%(asctime)s %(message)s')
+    logger = logging.getLogger(__name__)
+    return logger
+
+def dict_from_yaml(yml_file): 
+    return yaml.safe_load(open(yml_file, 'r'))
+
+def dict_to_yaml(dict_t, yml_file): 
+    with open(yml_file, 'w') as fp: 
+        yaml.dump(dict_t, fp, default_flow_style=False)
+
+class yml_base(object): 
+    def dump_yaml(self, cfg_path: PathLike) -> None: 
+        dict_to_yaml(self.get_setup(), cfg_path)
+
+def create_path(dir_type='md', sys_label=None, create_path=True): 
+    """
+    create MD simulation path based on its label (int), 
+    and automatically update label if path exists. 
+    """
+    dir_path = f'{dir_type}_run'
+    if sys_label: 
+        dir_path = f'{dir_path}_{sys_label}'
+    if create_path:
+        os.makedirs(dir_path, exist_ok=True)
+    return os.path.abspath(dir_path)
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c", "--config", help="YAML config file", type=str, required=True,
+    )
+    parser.add_argument(
+        "-d", "--dry_run", help="Option to only build all the ymls", 
+        type=bool, default=False, required=False,
+    )
+    args = parser.parse_args()
+    return args
+
+
+def trim_line(line):
+    return line.split()[0].replace('"', '')
+
+
+def find_diff(a, b):
+    """find elements that are in A, but not in B
+    """
+    return sorted([i for i in a if i not in b])
+
+
+def run_and_save(command, log):
+    tsk = subprocess.Popen(
+            command,
+            stdout=log, # subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            shell=True)
+    tsk.wait()
+
+
+def get_mismatch(a, b):
+    return find_diff(a, b), find_diff(b, a)
+
+
+def atomList_almostEqual(a, b):
+    if len(a) == len(b):
+        a, b = get_atomname(a), get_atomname(b)
+        # b = sorted([i[:-1] if len(i) > 1 else i for i in b])
+        x = [i != j for i, j in zip(a, b)]
+        if sum(x) == 0:
+            return True
+    return False
+
+
+def get_atomname(a):
+    """remove the last label in atomnames"""
+    return sorted([i[:-1] if len(i) > 1 else i for i in a])
+
+
+def get_ligand(pdb_file):
+    mda_trj = mda.Universe(pdb_file)
+    lig = mda_trj.select_atoms('not protein')
+    pdb_lig = os.path.abspath('./lig.pdb')
+    lig.write(pdb_lig)
+    return pdb_lig
+
+
+def get_protein(pdb_file):
+    mda_trj = mda.Universe(pdb_file)
+    lig = mda_trj.select_atoms('protein')
+    pdb_lig = os.path.abspath('./prot.pdb')
+    lig.write(pdb_lig)
+    return pdb_lig
+
+
+def get_lig_name(lig_pdb):
+    mda_u = mda.Universe(lig_pdb)
+    return mda_u.atoms[0].resname
+
+def update_pdb_obabel(pdb_file, format='pdb'):
+    """
+    add correct conect info to pdb structure 
+    obabel -ipdb lig.pdb -opdb >  lig_obabel.pdb
+    """
+    pdb_ob = pdb_file[:-4] + f'_ob.{format}'
+    subprocess.check_output(
+        f'obabel -ipdb {pdb_file} -o{format} >  {pdb_ob}',
+        shell=True)
+    return pdb_ob
+
+
+def get_formal_charge(pdb_file, format='pdb'): 
+    pdb_file = update_pdb_obabel(pdb_file, format=format)
+    if format == 'pdb':
+        mol = Chem.MolFromPDBFile(pdb_file)
+    elif format == 'mol2': 
+        mol = Chem.MolFromMol2File(pdb_file)
+    else: 
+        raise Exception("Unknown format...")
+    return Chem.GetFormalCharge(mol)
+
+
+def get_lig_charge(pdb_file): 
+    try:
+        lig_charge = get_formal_charge(pdb_file, format='mol2')
+    except: 
+        lig_charge = get_formal_charge(pdb_file, format='pdb')
+    n_electron = get_n_electron(pdb_file)
+    if (n_electron % 2 == 0) & (lig_charge % 2 == 0): 
+        return lig_charge 
+    elif (n_electron % 2 != 0) & (lig_charge % 2 != 0):
+        return lig_charge
+    elif n_electron % 2 == 0: 
+        warnings.warn(f"Using 0 for ligand charge. However, number of electron "\
+            f"{n_electron} and charge {lig_charge} "\
+            f"are mismatch for ligand {os.path.abspath(pdb_file)}")
+        return 0
+    else:
+        raise Exception(f"Number of electron {n_electron} and charge {lig_charge} "\
+            f"are mismatch for ligand {os.path.abspath(pdb_file)}")
+
+
+def get_n_electron(pdb_file): 
+    mda_u = mda.Universe(pdb_file)
+    n_ele = [element(atom.element).atomic_number for atom in mda_u.atoms]
+    return sum(n_ele)
+
+
+def is_protein(pdb_file):
+    mda_trj = mda.Universe(pdb_file)
+    not_prot = mda_trj.select_atoms('not protein')
+    if not_prot.n_atoms == 0:
+        return True
+    else:
+        return False
+
+
+def run_at_temp(func):
+    """
+    Run functions at a temp dir
+    """
+    def wrapper(*args, **kwargs):
+        current_dir = os.getcwd()
+        temp_path = tempfile.TemporaryDirectory()
+        os.chdir(temp_path.name)
+        output = func(*args, **kwargs)
+        os.chdir(current_dir)
+        return output
+    return wrapper
+
+
+def clean_pdb(pdb_file):
+    """
+    Remove all entris in pdb files other than `ATOM` and HETATM`
+    """
+    with open(pdb_file, 'r') as pdb:
+        pdb_atoms = [
+            line for line in pdb
+            if line.startswith('ATOM') or line.startswith('HETATM')]
+    with open(pdb_file, 'w') as pdb:
+        pdb.write(''.join(pdb_atoms))
+
+
+def to_pdb(pos_file, top_file, pdb_file):
+    top = pmd.load_file(top_file, xyz=pos_file)
+    top.write_pdb(pdb_file)
+
+
+def missing_hydrogen(pdb_file):
+    """
+    Check whether a pdb file contains H atoms
+
+    Parameters
+    ----------
+    pdb_file : str
+        path to input pdb file
+
+    Returns
+    -------
+    missingH : bool
+        True if missing H, false otherwise
+    """
+    trj = mda.Universe(pdb_file)
+    hydrogens = trj.select_atoms('name H*')
+    missingH = True if hydrogens.n_atoms == 0 else False
+    return missingH
+
+
+def remove_hydrogen(pdb_file, pdb_noH_file):
+    """
+    remove H atoms from a pdb file
+
+    Parameters
+    ----------
+    pdb_file : str
+        path to input pdb file
+    pdb_noH_file : str
+        path to write pdb file with H removed
+    """
+    trj = mda.Universe(pdb_file)
+    trj_noH = trj.select_atoms('not name H* and not name h*')
+    trj_noH.write(pdb_noH_file)
+
+
+def add_hydrogen(pdb_file):
+    """
+    add hydrogens to pdb structure if missing hydrogen atoms
+    obabel -ipdb adp.pdb -h -opdb >  adph.pdb
+    """
+    if not missing_hydrogen(pdb_file):
+        return pdb_file
+    else:
+        pdb_noH = pdb_file
+
+    pdb_h = pdb_file[:-4] + '_h.pdb'
+    subprocess.check_output(
+        f'obabel -ipdb {pdb_noH} -h -opdb >  {pdb_h}',
+        shell=True)
+    clean_pdb(pdb_h)
+    return pdb_h
+
+
+def align_to_template(pdb_file, ref_file, pdb_output):
+    """
+    align frame to target
+    """
+    pdb = mda.Universe(pdb_file)
+    ref = mda.Universe(ref_file)
+    _ = align.alignto(pdb, ref, select='protein and name CA')
+    pdb.atoms.write(pdb_output)
