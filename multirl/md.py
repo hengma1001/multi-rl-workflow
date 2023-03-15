fe3a4db4197abc4ba6afa93996e01fe92d792e97
Heng Ma <maheng1991@hotmail.com>
2023-03-15 12:04:56 -0700

    added md setup to md run functions

--- multirl/md.py
+++ multirl/md.py
@@ -1,16 +1,18 @@
 """Functions related to running and analyzing molecular dynamic trajectories"""
 from concurrent.futures import ProcessPoolExecutor
 import os
+from pathlib import Path
 
 from multirl.models import Sequence
 from multirl.utils import pin_then_run
+from multirl.sim.run import sim_eval
 
 
 class MolecularDynamicsResult(str):
     """Result from a molecular dynamics calculation (placeholder)"""
 
 
-def batch_run_molecular_dynamics(sequences: list[Sequence]) -> list[MolecularDynamicsResult]:
+def batch_run_molecular_dynamics(sequences: list[Sequence], md_yml: Path) -> list[MolecularDynamicsResult]:
     """Run molecular dynamics on each member of a batch of sequences
 
     Args:
@@ -25,7 +27,7 @@ def batch_run_molecular_dynamics(sequences: list[Sequence]) -> list[MolecularDyn
     with ProcessPoolExecutor(n_ranks) as exc:
         # Submit all sequences
         futures = [
-            exc.submit(pin_then_run, run_molecular_dynamics, rank, n_ranks, seq)
+            exc.submit(pin_then_run, run_molecular_dynamics, rank, n_ranks, seq, md_yml)
             for rank, seq in enumerate(sequences)
         ]
 
@@ -33,13 +35,16 @@ def batch_run_molecular_dynamics(sequences: list[Sequence]) -> list[MolecularDyn
         return [f.result() for f in futures]
 
 
-def run_molecular_dynamics(sequence: Sequence) -> MolecularDynamicsResult:
+def run_molecular_dynamics(sequence: Sequence, md_yml: Path) -> MolecularDynamicsResult:
     """Perform a molecular dynamics calculation on a sequence
 
     Args:
         sequence: Sequence to be evaluated
+        md_yml: Yaml file that stores MD simulation setup
     Returns:
         Result of the molecular dynamics
     """
     assert sequence is not None
+    pdb = ESMFold(sequence)
+    rmsf = sim_eval(pdb, md_yml)
     return MolecularDynamicsResult(os.environ.get("CUDA_VISIBLE_DEVICES"))
