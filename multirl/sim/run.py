e79e9c9946d6a215afe7a49223d829eac38497cf
Heng Ma <maheng1991@hotmail.com>
2023-03-15 11:47:11 -0700

    removed test main function

---
+++ multirl/sim/run.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import shutil
+import numpy as np
+
+sys.path.append("../")
+from multirl.sim.amber import AMBER_param
+from multirl.sim.sim import Simulate
+from multirl.sim.utils import dict_from_yaml, cal_rmsf
+
+def main(yml_file, pdb=None):
+    args = dict_from_yaml(yml_file)
+    if not pdb:
+        pdb = args['pdb_file']
+    pdb, top = param(pdb)
+
+    args['pdb_file'] = pdb
+    args['top_file'] = top
+    sim_path = sim(args)
+    dcd = f"{sim_path}/output.dcd"
+
+    rmsf = cal_rmsf(top, dcd)
+    np.save(f'{sim_path}/rmsf.npy', rmsf)
+    return rmsf
+
+
+def param(pdb):
+    host_dir = os.getcwd()
+
+    # label for ligand identity
+    pdb_code = os.path.basename(pdb)[:-4]
+
+    # make the work dir
+    work_dir = os.path.abspath(os.path.join(host_dir, 'input_' + pdb_code))
+    os.makedirs(work_dir, exist_ok=True)
+
+    # make a copy of pdb in the new dir
+    pdb_copy = os.path.join(work_dir, os.path.basename(pdb))
+    shutil.copy2(pdb, pdb_copy)
+
+    # run and get the parameters
+    os.chdir(work_dir)
+    amberP = AMBER_param(pdb_copy)
+    print(amberP.prot_files, amberP.lig_files)
+    pdb, top = amberP.param_comp()
+    os.chdir(host_dir)
+    return pdb, top
+
+def sim(args): 
+    sim = Simulate(**args)
+    return sim.md_run()
+
